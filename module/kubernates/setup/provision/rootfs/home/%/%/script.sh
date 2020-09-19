b7b17be2527ef2674b3334023a1f0fab9d1e8987
Mikhail Ryzhikov <i@developer.com.ua>
2020-09-19 15:57:14 +0300

    Kubernates module implementation

--- module/kubernates/setup/provision/rootfs/home/%/%/script.sh
+++ module/kubernates/setup/provision/rootfs/home/%/%/script.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#docker run -di --name example-container alpine
-#docker exec -i example-container echo "Good luck! :)"
+kubectl create deployment good-luck --image=hello-world:latest
 
 exit 0
