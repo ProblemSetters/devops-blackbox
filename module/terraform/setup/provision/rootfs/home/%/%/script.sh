dc1deeae0b24b0c9369b2b16daf8a805413ec90f
Mikhail Ryzhikov <i@developer.com.ua>
2021-04-26 06:46:10 +0300

    Terraform module implementation

--- module/terraform/setup/provision/rootfs/home/%/%/script.sh
+++ module/terraform/setup/provision/rootfs/home/%/%/script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-terraform init -backend=false -reconfigure
-terraform validate -json
-terraform apply -auto-approve -input=false
+terraform init -backend=false -force-copy -no-color -reconfigure
+terraform validate -json -no-color
+terraform apply -auto-approve -compact-warnings -input=false -no-color
 
 exit 0
